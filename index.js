a383f1c61be54916e0e70d93706fd2360f903995
Noah Wyche <noahwyche1@gmail.com>
2023-11-20 14:05:20 -0500

    Fleshed out the skeleton of the code in the index.js file.

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
+const axios = require('axios');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter the title of your project:',
+      },
+    {
+        type: '',
+        name: '',
+        message: '',
+    },
+    {
+        type: '',
+        name: '',
+        message: '',
+    },
+    {
+        type: '',
+        name: '',
+        message: '',
+    },
+    {
+        type: '',
+        name: '',
+        message: '',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeToFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log('Readme Generated Sucessfully!')
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init(ReadmeGenerator) {
+
+}
 
 // Function call to initialize app
 init();
